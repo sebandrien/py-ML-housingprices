52e904c4c8548983b7b3a49bf80e95a8696f499d
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-04-05 14:30:41 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd                              
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("housing.csv") #Reading in housing file
+
+data #Visualzing data
+
+data.info() #Displaying "data" info
