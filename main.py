e5a46ac3e663d80c067ba6991eff854aebdae398
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-04-06 00:06:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ data = pd.read_csv("housing.csv") #Reading in housing file
 data #Visualzing data
 
 data.info() #Displaying "data" info
+
+data.dropna(inplace=True) #Dropping null values from "data", done to properly train model
+
+data.info() #Displaying new "data"
+
+from sklearn.model_selection import train_test_split
+x = data.drop(['median_house_value'],axis=1)
+y = data['median_house_value']
+
+x_train,x_test,y_train,y_text = train_test_split(x,y,test_size=0.2)
+
+
+
+
